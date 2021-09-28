6429662d520d8fe557d2dfc3837213aedb8dada9
ManuelNevado <manuneva@ucm.es>
2021-09-28 14:32:24 +0200

    end function in board class

--- TresEnRayaServer/src/model/Board.java
+++ TresEnRayaServer/src/model/Board.java
@@ -47,21 +47,35 @@ public class Board {
 		}
 		return count;
 	}
-
+	
 	public boolean end() {
+		boolean end=false;
 		//Filas
 		for(int i=0;i<3;i++) {
-			
+			if(board[i][0]=='X') {
+				if(board[i][1]==board[i][2] && board[i][2]=='X')end =true;
+			}else if(board[i][0]=='Y') {
+				if(board[i][1]==board[i][2] && board[i][2]=='Y')end =true;
+			}
 		}
 		//Columnas
 		for(int i=0;i<3;i++) {
-			
+			if(board[0][i]=='X') {
+				if(board[0][i]==board[2][i] && board[2][i]=='X')end =true;
+			}else if(board[0][i]=='Y') {
+				if(board[1][i]==board[2][i] && board[2][i]=='Y')end =true;
+			}
 		}
 		//Diagonales
-		for(int i=0;i<3;i++) {
-			
+		if(board[0][0]!=' ') {
+			if(board[0][0] == board[1][1] && board[0][0] == board[2][2]) end=true;
 		}
-		return false;
+		if(board[2][0] != ' ') {
+			if(board[2][0] == board[1][1] && board[0][3] == board[2][2]) end=true;
+		}
+		
+		return end;
 	}
+	
 	
 }
